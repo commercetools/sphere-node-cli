0803b94119d238f338ad1b12ca51c061901b28b7
Selwyn <Siilwyn@users.noreply.github.com>
2017-01-09 16:08:10 +0100

    feat(cli): add orders update command

--- test/commands/import.spec.js
+++ test/commands/import.spec.js
@@ -359,7 +359,7 @@ test(`ImportCommand
   before().then((command) => {
     const spy1 = sinon.stub(command, '_stream')
     const spy2 = sinon.stub(command, '_preProcess', opts => command._process(
-          Object.assign(opts, { config: {}, credentials: fakeCredentials }))
+      Object.assign(opts, { config: {}, credentials: fakeCredentials }))
     )
     command.run(['node', `${BIN_DIR}/sphere-import`,
       '-p', 'foo', '-t', 'productType', '-f', './foo.json'])
@@ -387,6 +387,45 @@ test(`ImportCommand
       credentials: fakeCredentials,
     }, 'all flags are passed')
     t.equal(spy1.args[0][1], 'productTypes.*', 'type value is passed')
+    t.equal(spy1.args[0].length, 4, 'Correct number of args is parsed')
+    t.ok(spy2.calledOnce, '_preProcess is calledOnce')
+    t.end()
+  }).catch(t.fail)
+})
+
+test(`ImportCommand
+  should process order command`, (t) => {
+  before().then((command) => {
+    const spy1 = sinon.stub(command, '_stream')
+    const spy2 = sinon.stub(command, '_preProcess', opts => command._process(
+      Object.assign(opts, { config: {}, credentials: fakeCredentials }))
+    )
+    command.run(['node', `${BIN_DIR}/sphere-import`,
+      '-p', 'foo', '-t', 'order', '-f', './foo.json'])
+    t.equal(
+      command.program.project,
+      'foo',
+      'project flag is parsed from the cli'
+    )
+    t.equal(
+      command.program.type,
+      'order',
+      'type flag is parsed from the cli'
+    )
+    t.equal(
+      command.program.from,
+      './foo.json',
+      'from flag is parsed from the cli'
+    )
+    t.deepEqual(spy1.args[0][0], {
+      project: 'foo',
+      type: 'order',
+      from: './foo.json',
+      batch: 5,
+      config: {},
+      credentials: fakeCredentials,
+    }, 'all flags are passed')
+    t.equal(spy1.args[0][1], '*', 'type value is passed')
     t.equal(spy1.args[0].length, 4, 'Correct number of args is parsed')
     t.ok(spy2.calledOnce, '_preProcess is calledOnce')
     t.end()
